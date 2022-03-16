6e7d3ad48c61a6e4c61104ad28d14cf05777bf83
debbyne <kinyadebbyne@gmail.com>
2022-03-16 10:56:29 +0300

    add styling to homepage

--- app/main/forms.py
+++ app/main/forms.py
@@ -9,7 +9,7 @@ class NewBlogForm(FlaskForm):
     submit = SubmitField('Submit')
 
 class CommentForm(FlaskForm):
-  comment = TextAreaField('Comments',validators=[DataRequired()],  render_kw={"placeholder": "Type a comment..."} )
+  comment = TextAreaField(validators=[DataRequired()],  render_kw={"placeholder": "Type a comment..."} )
   submit = SubmitField('Submit')
 
 class UpdateProfilePicture(FlaskForm):
